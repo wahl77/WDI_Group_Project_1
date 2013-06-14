3c309c605c7d7500e9326891720baef948f69197
Franky W. (franklinwahl - Frankys-MacBook-Pro.local) <wahl77+git@gmail.com>
2013-06-14 11:39:01 -0700

    Initialized empty rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WDIGroupProject1::Application.config.secret_token = 'f71a5cc31d46d6e101201f9730209ba6e8839d60aad7608c56e8225840942012f8e57d9be32b5ba998effbecfa2cee217fb34544c201f30eb8f4edf5455653a6'
